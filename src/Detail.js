f326fcf407e2a532c0501b159f5ec518e817c330
gilsun <gilsun0821@gmail.com>
2021-11-07 23:34:08 -0800

    add input and useEffect

--- src/Detail.js
+++ src/Detail.js
@@ -1,7 +1,30 @@
-/* eslint-disable*/
+import { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
+import styled from "styled-components";
+import "./Detail.scss";
+
+let 박스 = styled.div`
+  padding: 30px;
+`;
+let 제목 = styled.h4`
+  font-size: 25px;
+  color: ${(props) => props.색상};
+`;
 
 const Detail = (props) => {
+  const [창보기, 창설정] = useState(true);
+  const [input,setInput]=useState('')
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      창설정(false);
+    }, 2000);
+    console.log('안녕')
+    return () => {
+      clearTimeout(timeout);
+    };
+   
+  },[창보기]);
+
   let { id } = useParams();
   let history = useHistory();
 
@@ -11,6 +34,19 @@ const Detail = (props) => {
 
   return (
     <div className="container">
+      <박스>
+        <제목 className="red"> Detail</제목>
+      </박스>
+
+      <input onChange={(e)=>{setInput(e.target.value)}}/>
+
+      <h3>{input}</h3>
+      {창보기 === true && (
+        <div className="my-alert">
+          <p>재고가 얼마 남지 않았습니다. </p>
+        </div>
+      )}
+
       <div className="row">
         <div className="col-md-6">
           <img src={item.url} width="100%" alt={item.title} />
